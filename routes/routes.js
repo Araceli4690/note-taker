55ac9e567ece398e73ccd40825783200f031d362
araceli <carmona.araceli.4690@gmail.com>
2021-07-06 12:53:26 -0700

    port not displaying page

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 //dependencies
+const { response } = require('express');
 const fs = require('fs');
 const path = require('path');
 
@@ -6,9 +7,36 @@ const path = require('path');
 module.exports = app => {
 // html routes
     app.get('/notes', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+        res.sendFile(path.join(__dirname, './develop/public/notes.html'));
     });
     app.get('*', function(req,res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        res.sendFile(path.join(__dirname, './develop/public/index.html'));
     });
+
+//api get&post route
+    fs.readFile('./develop/db/db.json','utf8', (err, data) => {
+        if (err) {
+            return console.log(err);
+        };
+
+        let notes = JSON.parse(data);
+
+        console.log('returning notes data:' + JSON.stringify(notes));
+
+        app.get('/api/notes', (req, res) => {
+            res.json(notes);
+        });
+
+        app.post('/api/notes', (req, res) => {
+            
+            let newNote = req.body;
+            console.log('New Note Created:' + JSON.stringify(newNote));
+
+            notes.push(newNote);
+
+            fs.writeFile('./develop/db/db.json',JSON.stringify(notes));
+            //res.json(notes);
+        });
+    })
+
 };
--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const path = require("path");
 // port & initialize app 
 const app = express();
 const port = process.env.PORT || 3001;
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log('API server now on port 3001!');
 });
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 //setting express for data parsing 
 app.use(express.urlencoded({extended: true}));
