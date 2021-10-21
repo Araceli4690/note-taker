8904e6062dfe482c4a840c30dc56f48a6a5662f7
araceli <carmona.araceli.4690@gmail.com>
2021-10-20 18:49:24 -0700

    added delete and get by id route to routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,15 +2,11 @@
 const fs = require('fs');
 const path = require('path');
 
-//creating uuid
-const {v4: uuidv4} = require('uuid');
-uuidv4();
-
 
 module.exports = app => {
 
-//api get&post route
-    fs.readFile('./db/db.json','utf8', (err, data) => {
+    //api get&post route
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
 
         let notes = JSON.parse(data);
 
@@ -21,28 +17,36 @@ module.exports = app => {
         });
 
         app.post('/api/notes', (req, res) => {
-            
+
             let newNote = req.body;
             //unique id created
-            newNote.id = uuidv4();
+
             notes.push(newNote);
 
             //updating db
             newNotes();
-            
+
             console.log('New Note Created:' + JSON.stringify(newNote));
         });
+        app.get('/api/notes/:id', (req, res) => {
+            res.json(notes[req.params.id, 1])
+        })
+        app.delete('/api/notes/:id', (req, res) => {
+            notes.splice(req.params.id, 1);
+            newNotes();
+            console.log('Deleted note')
+        })
         //html routes moved after api routes
-        app.get('/notes', function(req, res) {
+        app.get('/notes', function (req, res) {
             res.sendFile(path.join(__dirname, '../public/notes.html'));
         });
-        app.get('*', function(req,res) {
+        app.get('*', function (req, res) {
             res.sendFile(path.join(__dirname, '../public/index.html'));
         });
-        
+
         //function to update db
         function newNotes() {
-            fs.writeFile("./db/db.json",JSON.stringify(notes),err => {
+            fs.writeFile("db/db.json", JSON.stringify(notes), err => {
                 if (err) {
                     return console.log(err);
                 }
